3daaccea462cf7e29a5f48fb3f4ab173030d866f
傅杰 <1516351335@qq.com>
2019-03-27 10:17:13 +0800

    Branch1第1次提交

--- app/src/main/java/com/example/uart1/MainActivity.java
+++ app/src/main/java/com/example/uart1/MainActivity.java
@@ -237,7 +237,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-                //保存配置(第13次提交)
+                //保存配置(Branch1第1次提交)
 //                save("baudRate="+baudRate);
                 Toast.makeText(MainActivity.this, "保存成功!",
                         Toast.LENGTH_SHORT).show();
